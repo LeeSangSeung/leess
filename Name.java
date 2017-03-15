b96d41914b2c087e60a13445140cce642b327350
leess <ppurelee@gmail.com>
2017-03-16 05:48:18 +0900

    Introduce

--- Name.java
+++ Name.java
@@ -6,13 +6,16 @@ public class Name
 	{
 		int x = 0;
 		
-		while(x!=1)
+		while(x!=1 && x!=2)
 		{
 			Scanner input = new Scanner(System.in);
 			x = input.nextInt();
-			System.out.println("input 1");
 		}
 		
-		System.out.println("Lee SangSeung");
+		if(x==1)
+			System.out.println("My name is Lee SangSeung");
+		
+		else if(x==2)
+			System.out.println("My major is Computer Science Engineering");
 	}
 }
