34c6dca442dcd9f2895e0a71c5b9599361d5aa1a
leess <ppurelee@gmail.com>
2017-03-16 04:37:55 +0900

    Name

---
+++ Name.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Name 
+{
+	public static void main(String args[]) 
+	{
+		int x = 0;
+		
+		while(x!=1)
+		{
+			Scanner input = new Scanner(System.in);
+			x = input.nextInt();
+			System.out.println("1을 입력하세요.");
+		}
+		
+		System.out.println("이상승");
+	}
+}
